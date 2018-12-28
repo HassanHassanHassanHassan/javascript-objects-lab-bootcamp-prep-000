3dcbc9f2b740df56fe676b18e74975f5b85c244e
compiled-lab-0043 <chaudhy.hassan@yahoo.com>
2018-12-28 03:29:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateObjectWithKeyAndValue (object, key, value) {
 object[key] = value
 return object
  }
- var obj = { foo: 'bar' }
+// var obj = { foo: 'bar' }
  
 var newObj = Object.assign({}, recipes)
 delete newObj.foo
